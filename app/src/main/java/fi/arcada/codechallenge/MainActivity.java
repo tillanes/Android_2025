3c9d7f77dce99c1daccc08b33d4074ac0b93335b
samuel Tillanen <tillanes@arcada.fi>
2025-03-25 16:58:10 +0200

    lektion 2

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,12 +1,11 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+    }
+    public void test() {
+        ArrayList<Double> numbers = new ArrayList<Double>();
+
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+    }
+
+
+
+
+    public void calculate(View view) {
+        System.out.println("toimii");
+        numbers.add(15.0);
     }
 }
