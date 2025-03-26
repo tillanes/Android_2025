f5feed29f0637b719d6825315de05a1984aa911c
Samuel Tillanen <tillanes@arcada.fi>
2025-03-26 11:47:35 +0200

    Code Challenge Mean()

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -2,12 +2,19 @@ package fi.arcada.codechallenge;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+    public ArrayList<Double> numbers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void test() {
-        ArrayList<Double> numbers = new ArrayList<Double>();
 
+
+
+    public void test() {
         numbers.add(1.0);
         numbers.add(2.0);
         numbers.add(3.0);
@@ -30,9 +38,45 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     public void calculate(View view) {
-        System.out.println("toimii");
-        numbers.add(15.0);
+        test();
+       // System.out.println("toimii");
+
+
+
+        System.out.println(numbers);
+
+
+        statisctics stats = new statisctics();
+
+        stats.calcMean();
+
+
     }
+
+    public class statisctics {
+        public void calcMean() {
+
+            double sum = 0;
+
+            for (Double num : numbers) {
+                sum += num;
+            }
+
+            double mean = sum / numbers.size();
+
+            System.out.println("The mean is: " + mean);
+
+           // System.out.println("class tomii");
+
+            String StrMean = Double.toString(mean);
+            TextView field = (TextView) findViewById(R.id.textView);
+            field.setText(StrMean);
+
+        }
+
+
+    }
+
+
 }
